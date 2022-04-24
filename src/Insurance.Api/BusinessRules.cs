68355b980418185636e4be2afac274437251daeb
Mohsen Afshin <mafshin89@gmail.com>
2022-04-24 11:11:33 +0200

    Added checking if product has insurance before calculation

--- src/Insurance.Api/BusinessRules.cs
+++ src/Insurance.Api/BusinessRules.cs
@@ -8,20 +8,21 @@ namespace Insurance.Api
         {
             float insurance = 0f;
 
-            if (toInsure.SalesPrice < 500)
-                insurance = 0;
-            else
+            if (toInsure.ProductTypeHasInsurance)
             {
-                if (toInsure.SalesPrice > 500 && toInsure.SalesPrice < 2000)
-                    if (toInsure.ProductTypeHasInsurance)
-                        insurance += 1000;
-                if (toInsure.SalesPrice >= 2000)
-                    if (toInsure.ProductTypeHasInsurance)
-                        insurance += 2000;
-            }
+                if (toInsure.SalesPrice < 500)
+                    insurance = 0;
+                else
+                {
+                    if (toInsure.SalesPrice > 500 && toInsure.SalesPrice < 2000)
+                            insurance += 1000;
+                    if (toInsure.SalesPrice >= 2000)
+                            insurance += 2000;
+                }
 
-            if ((toInsure.ProductTypeName == "Laptops" || toInsure.ProductTypeName == "Smartphones") && toInsure.ProductTypeHasInsurance)
-                insurance += 500;
+                if (toInsure.ProductTypeName == "Laptops" || toInsure.ProductTypeName == "Smartphones")
+                    insurance += 500;
+            }
 
             return insurance;
         }
